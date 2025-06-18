2e7c4069073e7a9a4fcec5559d49ad36b2983b6c
steven <scoaila@proton.me>
2025-06-17 19:37:39 -0500

    Initial Commit

---
+++ BuenMouse/BuenMouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuenMouseApp.swift
+//  BuenMouse
+//
+//  Created by Steven Coaila Zaa on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BuenMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuenMouseTests/BuenMouseTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuenMouseTests.swift
+//  BuenMouseTests
+//
+//  Created by Steven Coaila Zaa on 17/06/25.
+//
+
+import Testing
+@testable import BuenMouse
+
+struct BuenMouseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
